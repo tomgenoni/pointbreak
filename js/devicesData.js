3da392bc6caa8f171dc067883acb1725e7b64c78
Tom Genoni <tom@atomeye.com>
2016-11-01 21:36:41 -0700

    Adding proper x symbol

--- js/devicesData.js
+++ js/devicesData.js
@@ -1,12 +1,27 @@
 devicesData = [
   {
-    title: 'iPhone',
+    title: 'iPhone 5',
+    width: 320,
+    height: 568
+  },
+  {
+    title: 'iPhone 6',
     width: 400,
     height: 500
   },
   {
-    title: 'Shoebox',
+    title: 'iPhone 6s',
     width: 400,
     height: 500
   },
+  {
+    title: 'iPhone 7',
+    width: 375,
+    height: 667
+  },
+  {
+    title: 'iPhone 7 Plus',
+    width: 414,
+    height: 736
+  }
 ]
--- js/renderTemplate.js
+++ js/renderTemplate.js
@@ -8,7 +8,7 @@ function renderTemplate(templateName, target, data, type) {
       var template =`
       <div class="token__item" data-id="${data[index].id}" data-title="${data[index].title}" data-width="${data[index].width}" data-height="${data[index].height}">
         <div class="token__text">
-          <div class="token__size">${data[index].width}x${data[index].height}</div>
+          <div class="token__size">${data[index].width}×${data[index].height}</div>
           <div class="token__title">${data[index].title}</div>
           </div>
           <svg class="icon icon--small">
@@ -22,7 +22,7 @@ function renderTemplate(templateName, target, data, type) {
       var template =`
       <div class="view__item is-hidden" data-id="${data[index].id}">
         <div class="view__tools">
-            <span class="view__size">${data[index].width}x${data[index].height}</span>
+            <span class="view__size">${data[index].width}×${data[index].height}</span>
             <span class="view__title">${data[index].title}</span>
         </div>
         <div class="load-indicator"></div>
