462ae0d9a22d21629e11e4c24723d974176cc403
Tom Genoni <tom@atomeye.com>
2016-11-01 21:52:08 -0700

    Adding devices

--- js/devicesData.js
+++ js/devicesData.js
@@ -1,27 +1,42 @@
 devicesData = [
   {
-    title: 'iPhone 5',
+    title: 'iPhone 5/SE',
     width: 320,
     height: 568
   },
   {
-    title: 'iPhone 6',
-    width: 400,
-    height: 500
-  },
-  {
-    title: 'iPhone 6s',
-    width: 400,
-    height: 500
-  },
-  {
-    title: 'iPhone 7',
+    title: 'iPhone 6/7',
     width: 375,
     height: 667
   },
   {
-    title: 'iPhone 7 Plus',
+    title: 'iPhone 6/7 Plus',
     width: 414,
     height: 736
+  },
+  {
+    title: 'iPad',
+    width: 768,
+    height: 1024
+  },
+  {
+    title: 'Galaxy S 3/4',
+    width: 360,
+    height: 640
+  },
+  {
+    title: 'Google Nexus 4',
+    width: 384,
+    height: 640
+  },
+  {
+    title: 'Galaxy Note',
+    width: 400,
+    height: 640
+  },
+  {
+    title: 'Galaxy Tab',
+    width: 800,
+    height: 1280
   }
 ]
